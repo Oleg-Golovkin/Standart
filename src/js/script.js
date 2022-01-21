530e960da7e59a29be4b2a10adfb6a9b638831c8
Oleg <olegjan9@rambler.ru>
2022-01-21 10:23:20 +0300

    21.01.2022

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+import Calculator from "./modules/calculator";
+document.addEventListener("DOMContentLoaded", () => {
+    /* После полной загрузки HTML выполняется JS */
+    Calculator();
+
+});
